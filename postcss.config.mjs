8285c1aee08133a5aa65cb297bdd87a6e6185cc7
Guney Unay <guneymarketing17@hotmail.com>
2024-09-11 17:31:34 +0300

    :hammer: update postcss.config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    'postcss-pxtorem': {
+      rootValue: 16,
+      propList: ['*'],
+    },
     tailwindcss: {},
   },
-};
+}
 
-export default config;
+export default config
